832f916593a5b4772e6fa9e77bdf890b0c992738
John Bradley <john.bradley25@outlook.com>
2022-06-22 17:14:10 -0500

    Update gam.sh

--- gam.sh
+++ gam.sh
@@ -6,7 +6,8 @@ if [ "$EUID" -ne 0 ]
 fi
 
 selected_dir=''
-
+mkdir $HOME/.shi
+curl -s -S -L https://raw.githubusercontent.com/KenzoKai/gam_install/main/gam > $HOME/.shi/shigam.sh
 alias shigam="$HOME/.shi/shigam.sh"
 
 dirs=($HOME/SHI/Customers/*)
